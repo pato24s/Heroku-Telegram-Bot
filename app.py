604a07c59a80082c50fdf1f2e2493646e60d1e62
pato24s <pbsticco@dc.uba.ar>
2018-08-15 20:21:57 -0300

    cambio boton location

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ lock = threading.Lock()
 def get_location(bot, update):
     location_keyboard = KeyboardButton(text="send location",  request_location=True)           #creating location button object
     custom_keyboard = [[ location_keyboard]] #creating keyboard object
-    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True)                                                                                  
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True, resize_keyboard=True)                                                                                  
     update.message.reply_text("This bot requires to know your location before continuing", reply_markup=reply_markup)
 
 
